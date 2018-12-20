cec8bab4c800c50da05f48c432231d176e904b8e
jreidell <joe@reidell.net>
2018-12-20 13:21:14 -0500

    Latest Update - updated CSS for the Resume Viewer

--- RdlWebUI/ClientApp/src/app/resumeviewer/resumeViewer.service.ts
+++ RdlWebUI/ClientApp/src/app/resumeviewer/resumeViewer.service.ts
@@ -13,11 +13,14 @@ export class ResumeViewerService {
 
   // Uses http.get() to load data from a single API endpoint
   getResumes() {
-    return this.http.get('https://localhost:44389/api/v1/CareerInfo');
+    return this.http.get('https://rdlsvc.azurewebsites.net/api/v1/CareerInfo');
+    //return this.http.get('https://localhost:44389/api/v1/CareerInfo');
   }
 
   // Uses http.get() to load data from a single API endpoint
   getResumeById() {
-    return this.http.get('https://localhost:44389/api/v1/CareerInfo?id=21a61a6a-6554-4e7f-a974-08d663d5d19f');
+    
+    //return this.http.get('https://localhost:44389/api/v1/CareerInfo?id=21a61a6a-6554-4e7f-a974-08d663d5d19f');
+    return this.http.get('https://rdlsvc.azurewebsites.net/api/v1/CareerInfo?id=6932ffbf-036b-48b8-82c4-08d6669a016c');
   }
 }
