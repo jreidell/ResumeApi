85304f6d9e39bd49130211b2da34dee3823f44f0
jreidell <joe@reidell.net>
2018-12-21 16:09:55 -0500

    updating resume guid while working to secure the application and the code

--- RdlWebUI/ClientApp/src/app/resumeviewer/resumeViewer.service.ts
+++ RdlWebUI/ClientApp/src/app/resumeviewer/resumeViewer.service.ts
@@ -21,6 +21,6 @@ export class ResumeViewerService {
   getResumeById() {
     
     //return this.http.get('https://localhost:44389/api/v1/CareerInfo?id=21a61a6a-6554-4e7f-a974-08d663d5d19f');
-    return this.http.get('https://rdlsvc.azurewebsites.net/api/v1/CareerInfo?id=6932ffbf-036b-48b8-82c4-08d6669a016c');
+    return this.http.get('https://rdlsvc.azurewebsites.net/api/v1/CareerInfo?id=58f21038-a7e4-46ec-b036-08d667882bcb');
   }
 }
