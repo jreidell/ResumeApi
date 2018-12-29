a8c703286dff1aeb20fa2490761d45fae504d014
jreidell <joe@reidell.net>
2018-12-28 21:02:01 -0500

    Added a new React JS Project to the mix to further diversify the UI demonstrations

---
+++ RdlNetReact/ClientApp/src/components/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  displayName = Home.name
+
+  render() {
+    return (
+      <div>
+        <h1>Reidell.Net - ASP.NET Core, Web API, REST, LINQ, AzureSQL (MSSQL), REACT, Angular, Bootstrap, JSON, Azure App Services, Xamarin, Android</h1>
+        <p><strong>Welcome to my demo, single-page application, built with:</strong></p>
+        <ul>
+          <li><a href='https://azure.microsoft.com/en-us/'>Microsoft Azure</a> cloud, using Azure Application Services</li>
+          <li>A REST API built on <a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+          <li><a href="https://azure.microsoft.com/en-us/services/sql-database/">Microsoft Azure SQL Server</a> for Data Storage</li>
+          <li>One of the UIs is built with <a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+          <li>One of the UIs is built with <a href='https://angular.io/'>Angular v5.x</a> and <a href='http://www.typescriptlang.org/'>TypeScript</a> for client-side code</li>
+          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li>Review the Source Code for this application via my public <a href='https://github.com/jreidell/ResumeApi'>GitHub Repository</a></li>
+        </ul>
+            <p><strong>Some technologies that are showcased within this application are:</strong></p>
+        <ul>
+          <li><strong>Client-side navigation</strong>. For example, click <em>Reume Viewer</em> then <em>Back</em> to return here.</li>
+          <li>An implementation of an Asynchronous <strong>Repository Pattern</strong> to add a layer of abstraction</li>
+          <li>An implementation of the <strong>Unit of Work</strong> Pattern</li>
+          <li>IoC or Inversion of Control is implemented by way of <strong>Dependency Injection</strong></li>
+          <li>A <strong>RESTful Web API</strong> is used to expose the <strong>Microsoft AzureSQL</strong> data as <strong>JSON</strong> Entities</li>
+          <li>I have also added a set of <strong>Xamarin, Cross-Platform, Mobile</strong> User Interface Applications to the <a href='https://github.com/jreidell/ResumeApi'>GitHub Repository</a></li>
+          <li>I will soon be publishing an <strong>Android Application</strong> to the <strong>Google Play Store</strong></li>
+        </ul>
+        <p>Click the Resume Viewer link in the Navigation Menu to view realtime resume data from a cloud-based MSSQL Server Instance via an ASP.NET Core, Web API. UI Application is built on Angular version 5. This application is a real world representation of my actual resume in object form.</p>
+      </div>
+    );
+  }
+}
---
+++ RdlNetReact/ClientApp/src/components/ResView.js
@@ -0,0 +1,75 @@
+﻿import React, { Component } from 'react';
+import moment from 'moment';
+
+export class ResView extends Component {
+    displayName = ResView.name
+
+    constructor(props) {
+        super(props);
+        this.state = { careerInfos: [], loading: true };
+
+        fetch('https://rdlsvc.azurewebsites.net/api/v1/CareerInfo?id=58f21038-a7e4-46ec-b036-08d667882bcb')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ careerInfos: data, loading: false });
+            });
+    }
+
+    static renderResumeDiv(careerInfos) {
+        return (
+            <table className='table'>
+                <tbody>
+                    {careerInfos.map(careerInfo =>
+                        <tr key={careerInfo.CareerInfoId}>
+                            <td>
+                                <h2>{careerInfo.FirstName} {careerInfo.MiddleName} {careerInfo.LastName}, {careerInfo.Suffix}</h2>
+                                <h6>{careerInfo.Address1}, {careerInfo.City}, {careerInfo.State} {careerInfo.PostalCode}</h6>
+                                <h6>{careerInfo.EmailAddress}, {careerInfo.Phone}, Mobile: {careerInfo.Mobile}, </h6>
+                                <hr />
+                                <h4>{careerInfo.CareerInfoTitle}</h4>
+                                <hr />
+                                <p>{careerInfo.Summary}</p>
+                                <hr />
+                                <ul id="limheight">
+                                    {careerInfo.JobSkills.map(jobSkill =>
+                                        <li key={jobSkill.JobSkillId}>{jobSkill.JobSkillTitle}</li>
+                                    )}
+                                </ul>
+                                <hr />
+                                    {careerInfo.WorkHistory.map(workHistory =>
+                                    <ul key={workHistory.WorkHistoryId} id="jobTitleSpacer">
+                                        <li class="empListing">{workHistory.Employer}, {moment(workHistory.StartDate).format("MMM YYYY")} - {moment(workHistory.EndDate).format("MMM YYYY")}</li>
+                                        <li class="jobTitle">{workHistory.JobTitle}</li>
+                                        <li class="jobDescription">{workHistory.JobDescription}</li>
+                                        <li class="noDotLi">
+                                            <ul id="jobDetailSpacer">
+                                                {workHistory.WorkHistoryDetails.map(workHistoryDetail =>
+                                                    <li class="workHistoryDetail" key={workHistoryDetail.WorkHistoryDetailId}>{workHistoryDetail.ContentBody}</li>
+                                                )}
+                                            </ul>
+                                        </li>
+                                        <hr />
+                                    </ul>
+                                    )}
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : ResView.renderResumeDiv(this.state.careerInfos);
+
+        return (
+            <div>
+                <h1>Resume Viewer</h1>
+                <p>This component demonstrates viewing the resume data via React.</p>
+                {contents}
+            </div>
+        );
+    }
+}
